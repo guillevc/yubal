85c3ae66c08601e58c9b289c06b9e78f6746a7f8
guillevc <guille@guillevc.dev>
2026-03-04 14:55:09 +0100

    chore(web): migrate eslint config to use defineConfig

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,11 +1,12 @@
 import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import prettier from "eslint-config-prettier";
 
-export default tseslint.config(
+export default defineConfig(
   { ignores: ["dist", "node_modules", "src/api/schema.d.ts"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
